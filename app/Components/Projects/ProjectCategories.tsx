48da559bd805a5eb2b2e48b94a02d5da5e24dc86
takumisou <mochzackyaja009@gmail.com>
2025-02-10 23:26:04 +0700

    feat: add product sorting card

--- app/Components/Projects/ProjectCategories.tsx
+++ app/Components/Projects/ProjectCategories.tsx
@@ -3,28 +3,90 @@ import React, { ReactEventHandler } from "react";
 interface ListProps {
   content: string;
   event: ReactEventHandler;
+  color: string;
 }
 
-const ListComponent: React.FC<ListProps> = ({ content, event }) => {
-  return <button onClick={() => event}>{content}</button>;
+const ListComponent: React.FC<ListProps> = ({ content, event, color }) => {
+  return (
+    <button
+      onClick={event}
+      className={`px-4 hover:bg-colorPrimary py-2 w-full font-medium text-colorBlack flex justify-start items-start rounded-full hover:text-white ${color}`}
+    >
+      {content}
+    </button>
+  );
 };
 
 interface ProjectCategoriesProps {
   contentAll: string;
   eventAll: ReactEventHandler;
+  colorAll: string;
+  contentFirst: string;
+  eventFirst: ReactEventHandler;
+  colorFirst: string;
+  contentSecond: string;
+  eventSecond: ReactEventHandler;
+  colorSecond: string;
+  contentThird: string;
+  eventThird: ReactEventHandler;
+  colorThird: string;
+  contentFour: string;
+  eventFour: ReactEventHandler;
+  colorFour: string;
+  contentFive: string;
+  eventFive: ReactEventHandler;
+  colorFive: string;
 }
 
 const ProjectCategories: React.FC<ProjectCategoriesProps> = ({
   contentAll,
   eventAll,
+  colorAll,
+  contentFirst,
+  eventFirst,
+  colorFirst,
+  contentSecond,
+  eventSecond,
+  colorSecond,
+  contentThird,
+  eventThird,
+  colorThird,
+  contentFour,
+  eventFour,
+  colorFour,
+  contentFive,
+  eventFive,
+  colorFive,
 }) => {
   return (
-    <ul className="bg-white w-[50%] ml-12 rounded-xl px-12 flex flex-col gap-2 py-8">
-      <h2 className="text-2xl font-semibold cursor-pointer select-none">
-        Project Categories <i className="ri-arrow-down-s-line"></i>
-      </h2>
-      <ListComponent content={contentAll} event={eventAll} />
-    </ul>
+    <>
+      <ListComponent content={contentAll} event={eventAll} color={colorAll} />
+      <ListComponent
+        content={contentFirst}
+        event={eventFirst}
+        color={colorFirst}
+      />
+      <ListComponent
+        content={contentSecond}
+        event={eventSecond}
+        color={colorSecond}
+      />
+      <ListComponent
+        content={contentThird}
+        event={eventThird}
+        color={colorThird}
+      />
+      <ListComponent
+        content={contentFour}
+        event={eventFour}
+        color={colorFour}
+      />
+      <ListComponent
+        content={contentFive}
+        event={eventFive}
+        color={colorFive}
+      />
+    </>
   );
 };
 
