3397e739be8198a282a6c371b09b1fa13a4dde59
takumisou <mochzackyaja009@gmail.com>
2025-02-15 06:22:21 +0700

    feat: add Project Detail

--- app/Components/Projects/Pagination.tsx
+++ app/Components/Projects/Pagination.tsx
@@ -1,23 +1,38 @@
-const Pagination = () => {
+import CardList from "@/app/Json/CardList.json";
+
+interface arrowStep {
+  handlePagination: () => void;
+  stepOne: () => void;
+  stepTwo: () => void;
+}
+
+const Pagination: React.FC<arrowStep> = ({
+  stepOne,
+  stepTwo,
+  handlePagination,
+}) => {
   return (
     <div className="flex flex-row gap-2 justify-center items-center">
       <ul className="flex flex-row gap-2 cursor-pointer">
-        <li className="bg-white px-5 py-1 rounded-md font-medium hover:bg-colorSecondary/50">
-          1
-        </li>
-        <li className="bg-white px-5 py-1 rounded-md font-medium hover:bg-colorSecondary/50 ">
-          2
-        </li>
-        <li className="bg-white px-5 py-1 rounded-md font-medium hover:bg-colorSecondary/50">
-          3
-        </li>
-        <li className="bg-white px-5 py-1 rounded-md font-medium hover:bg-colorSecondary/50">
-          4
-        </li>
-        <li className="bg-white px-5 py-1 rounded-md font-medium hover:bg-colorSecondary/50">
+        {CardList.slice(0, 4).map((_, index) => (
+          <li
+            className="bg-white px-5 py-1 rounded-md font-medium hover:bg-colorSecondary/50 select-none"
+            onClick={handlePagination}
+            key={index}
+          >
+            {index + 1}
+          </li>
+        ))}
+        <li
+          className="bg-white px-5 py-1 rounded-md font-medium hover:bg-colorSecondary/50 select-none"
+          onClick={stepOne}
+        >
           <i className="ri-arrow-left-line"></i>
         </li>
-        <li className="bg-white px-5 py-1 rounded-md font-medium hover:bg-colorSecondary/50">
+        <li
+          className="bg-white px-5 py-1 rounded-md font-medium hover:bg-colorSecondary/50 select-none"
+          onClick={stepTwo}
+        >
           <i className="ri-arrow-right-line"></i>
         </li>
       </ul>
--- app/pages/Projects/Product.tsx
+++ app/pages/Projects/Product.tsx
@@ -10,8 +10,37 @@ import Link from "next/link";
 
 const Product = () => {
   const [projectCategories, setProjectCategories] = useState("");
+  const [stepOne, setStepOne] = useState(0);
+  const [stepTwo, setStepTwo] = useState(6);
 
-  // Filter daftar kartu berdasarkan kategori yang dipilih
+  // Handle Step Card List
+  const handleStep = (stepFirst: number, stepSec: number) => {
+    setStepOne(stepFirst);
+    setStepTwo(stepSec);
+  };
+
+  const nextStep = () => {
+    if (stepOne >= 6) {
+      handleStep(stepOne - 6, stepTwo - 6);
+    }
+  };
+
+  const prevStep = () => {
+    if (stepTwo < filteredCards.length) {
+      handleStep(stepOne + 6, stepTwo + 6);
+    }
+  };
+
+  const handlePagination = () => {
+    if (stepOne >= 6) {
+      handleStep(stepOne - 6, stepTwo - 6);
+    }
+    if (stepTwo < filteredCards.length) {
+      handleStep(stepOne + 6, stepTwo + 6);
+    }
+  };
+
+  // Filter Category Card
   const filteredCards = projectCategories
     ? CardList.filter((item) => item.categories === projectCategories)
     : CardList;
@@ -21,7 +50,7 @@ const Product = () => {
       {/* Filter Categories */}
       <div className="section-container flex flex-row gap-8">
         <nav className="w-[25%] gap-2 bg-white h-[50%] px-5 py-5 rounded-xl">
-          <h2 className="text-2xl font-semibold text-colorBlackSolid py-2 px-2 underline decoration-colorSecondary">
+          <h2 className="text-2xl font-bold text-colorBlackSolid py-2 px-2">
             Project Categories
           </h2>
           <ProjectCategories
@@ -29,67 +58,91 @@ const Product = () => {
             value=""
             id="all-categories"
             checked={projectCategories === ""}
-            changed={() => setProjectCategories("")}
+            changed={() => {
+              setProjectCategories("");
+              nextStep();
+            }}
           />
           <ProjectCategories
             content="Clinic Management System"
             value="Clinic-Management-System"
             id="Clinic-Management-System"
             checked={projectCategories === "Clinic-Management-System"}
-            changed={() => setProjectCategories("Clinic-Management-System")}
+            changed={() => {
+              setProjectCategories("Clinic-Management-System");
+              nextStep();
+            }}
           />
           <ProjectCategories
             content="Queue System"
             value="Queue-System"
             id="Queue-System"
             checked={projectCategories === "Queue-System"}
-            changed={() => setProjectCategories("Queue-System")}
+            changed={() => {
+              setProjectCategories("Queue-System");
+              nextStep();
+            }}
           />
           <ProjectCategories
             content="Human Capital Management"
             value="Human-Capital-Management-System"
             id="Human-Capital-Management-System"
             checked={projectCategories === "Human-Capital-Management-System"}
-            changed={() =>
-              setProjectCategories("Human-Capital-Management-System")
-            }
+            changed={() => {
+              setProjectCategories("Human-Capital-Management-System");
+              nextStep();
+            }}
           />
           <ProjectCategories
             content="Learning Management System"
             value="Learning-Management-System"
             id="Learning-Management-System"
             checked={projectCategories === "Learning-Management-System"}
-            changed={() => setProjectCategories("Learning-Management-System")}
+            changed={() => {
+              setProjectCategories("Learning-Management-System");
+              nextStep();
+            }}
           />
           <ProjectCategories
-            content="Project Management System"
-            value="Project-Management-System"
-            id="Project-Management-System"
-            checked={projectCategories === "Project-Management-System"}
-            changed={() => setProjectCategories("Project-Management-System")}
+            content="Total Relationship Management"
+            value="Total-Relationship-Management"
+            id="Total-Relationship-Management"
+            checked={projectCategories === "Total-Relationship-Management"}
+            changed={() => {
+              setProjectCategories("Total-Relationship-Management");
+              nextStep();
+            }}
           />
           <ProjectCategories
             content="GPS Tracking"
             value="GPS-Tracking"
             id="GPS-Tracking"
             checked={projectCategories === "GPS-Tracking"}
-            changed={() => setProjectCategories("GPS-Tracking")}
+            changed={() => {
+              setProjectCategories("GPS-Tracking");
+              nextStep();
+            }}
           />
         </nav>
 
         <div className="flex flex-col gap-8 justify-start items-start">
           <div className="flex flex-row gap-2 w-full">
             <SearchBar />
-            <Pagination />
+            <Pagination
+              handlePagination={() => handlePagination()}
+              stepOne={() => nextStep()}
+              stepTwo={() => prevStep()}
+            />
           </div>
 
           {/* Card Wrapper */}
-          <div className="grid grid-cols-3 gap-4 h-[90vh]">
+          <div className="grid grid-cols-3 gap-4 h-[100vh]">
             {filteredCards.length > 0 ? (
-              filteredCards.slice(0, 6).map((content, index) => {
+              filteredCards.slice(stepOne, stepTwo).map((content, index) => {
                 // const productFeatures = content.features.split(" ");
                 // const categories = content.categories.replace(/-/g, " ");
                 const advantageIcon = content.advantageIcon.split(" ");
+                const categories = content.categories.replace(/-/g, " ");
                 const advantage = content.advantage.split(" ");
                 const contentParams = content.name
                   .replace(/ /g, "-")
@@ -99,24 +152,25 @@ const Product = () => {
                   // Card
                   <div
                     key={index}
-                    className="w-[380px] h-[460px] bg-white rounded-xl"
+                    className="w-[380px] h-[490px] bg-white rounded-xl"
                   >
                     <Link href={`/projects/detail/${contentParams}`}>
                       <Image
                         src={content.thumbnail}
-                        width={380}
+                        width={880}
                         height={100}
                         alt="Card Image"
                         className="h-[24vh] object-cover rounded-t-xl cursor-pointer hover:opacity-90 animate"
                       />
                     </Link>
-                    <div className="h-[10vh] px-5 py-3 text-colorBlack gap-2 flex flex-col">
+                    <div className="h-[10vh] px-9 py-3 text-colorBlack gap-2 flex flex-col">
                       {/* Card Title */}
                       <div>
                         <h2 className="font-semibold text-xl">
                           {content.name}
                         </h2>
-                        <ul className="flex flex-row gap-2 items-center">
+                        <p className="text-sm font-medium">{categories}</p>
+                        <ul className="flex flex-row gap-2 items-center pt-2">
                           {advantage.map((content, index) => (
                             <li
                               key={index}
@@ -159,7 +213,7 @@ const Product = () => {
               })
             ) : (
               <p className="text-gray-500 text-lg">
-                Tidak ada data yang sesuai.
+                Tidak ada category project.
               </p>
             )}
           </div>
